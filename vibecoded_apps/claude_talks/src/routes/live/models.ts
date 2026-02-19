ca5ece34bccf662ce7acf2520c485d7566c4ecdb
Daniel Huynh <daniel.quoc.huynh@gmail.com>
2026-02-19 11:10:35 +0100

    Cleaned stuff

--- vibecoded_apps/claude_talks/src/routes/live/models.ts
+++
@@ -1,9 +0,0 @@
-export interface SessionInfo {
-  id: string;
-  name: string;
-  summary: string;
-  last_user_message: string;
-  last_assistant_message: string;
-  updated_at: string;
-  message_count: number;
-}
