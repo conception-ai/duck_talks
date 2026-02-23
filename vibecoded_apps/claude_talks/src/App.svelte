d9bb4bbd4e18aefc66a8021363a8b4e4b4509e86
Daniel Huynh <daniel.quoc.huynh@gmail.com>
2026-02-23 08:22:21 +0100

    Refactored frontend

--- vibecoded_apps/claude_talks/src/App.svelte
+++ vibecoded_apps/claude_talks/src/App.svelte
@@ -3,7 +3,6 @@
   import HomePage from './routes/home/+page.svelte';
   import LivePage from './routes/live/+page.svelte';
   import RecordingsPage from './routes/recordings/+page.svelte';
-  import NewUIPage from './routes/new-ui/+page.svelte';
 </script>
 
 <Router
@@ -12,6 +11,5 @@
     '/live': LivePage,
     '/live/:id': LivePage,
     '/recordings': RecordingsPage,
-    '/new-ui': NewUIPage,
   }}
 />
